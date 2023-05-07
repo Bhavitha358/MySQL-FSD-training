289bb7e8abd5c616bb928552eb7647099103aea6
Bhavitha358 <130962932+Bhavitha358@users.noreply.github.com>
2023-05-08 02:28:02 +0530

    Relational Operators.sql
    this is the sql file which contains conditional operations, arranging entries either in asc or desc using ORDER BY, dispalying the first n entries using LIMIT command.

--- SQL-3.sql
+++ SQL-3.sql
@@ -5,30 +5,49 @@ CREATE TABLE STUDENT(
     SMARKS INT NOT NULL
     );
 SELECT * FROM STUDENTS;
+-- Inserting values in the students table
 INSERT INTO STUDENTS VALUES (201,"ANU",87);
 INSERT INTO STUDENTS VALUES (202,"RAGHAV",75);
 INSERT INTO STUDENTS VALUES (203,"MODHAK",57);
 INSERT INTO STUDENTS VALUES (204,"SRIHAN",68);
 INSERT INTO STUDENTS VALUES (205,"RUPA",93);
 
+-- Find the details of the student who scored 68 marks.
 SELECT * FROM STUDENTS WHERE SMARKS = 68;
+-- Find all the details of the student who scored less than 68 marks.
 SELECT * FROM STUDENTS WHERE SMARKS < 68;
+-- Find all the details of the student who scored 68 and below marks.
 SELECT * FROM STUDENTS WHERE SMARKS <= 68;
-SELECT * FROM STUDENTS WHERE SMARKS >= 9068;
+-- Find the details of the student who scored 68  and above marks.
+SELECT * FROM STUDENTS WHERE SMARKS >= 68;
+-- Find all the details of the student who didnt score 68 marks.
 SELECT * FROM STUDENTS WHERE SMARKS != 68;
- SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80; 
- SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80 ORDER BY SMARKS;
-SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80 ORDER BY SNAME;
-SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80 ORDER BY SMARKS ASC;
-SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80 ORDER BY SMARKS DESC;
+                   (or)
 SELECT * FROM STUDENTS WHERE NOT SMARKS = 68;
+-- Find the details of the student who scored between 60 and 80 marks.
+ SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80; 
+-- Find all the details of the student who scored between 60 and 80 marks and arrange marks in ascending order.
+ SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80 ORDER BY SMARKS;
+-- Find all the details of the student who scored between 60 and 80 marks and arrange names in ascending order.
+SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80 ORDER BY SNAME;
+-- Find all the details of the student who scored between 60 and 80 marks and arrange marks in ascending order.
+SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80 ORDER BY SMARKS ASC;
+-- Find all the details of the student who scored between 60 and 80 marks and arrange marks in descending order.
+SELECT * FROM STUDENTS WHERE SMARKS BETWEEN 60 AND 80 ORDER BY SMARKS DESC;
+-- find the unique address of the student in RGMLearners table.
 SELECT DISTINCT  STD_ADDRESS FROM RGMLEARNERS;
+-- find the count of the unique address of the student in RGMLearners table.
 SELECT COUNT(DISTINCT  STD_ADDRESS) FROM RGMLEARNERS;
+-- find the count of the unique colleges of the student in RGMLearners table.
 SELECT COUNT(DISTINCT  STD_COLLEGE) FROM RGMLEARNERS;
 SELECT * FROM STUDENTS;
+-- find the first 3 entries of the table
 SELECT * FROM RGMLEARNERS LIMIT 3;
+-- find the first two entries of the table whose address is "Hyderabad".
 SELECT * FROM RGMLEARNERS WHERE STD_ADDRESS = "HYDERABAD" LIMIT 2;
-SELECT * FROM STUDENTS WHERE STD_ADDRESS = "HYDERABAD" LIMIT 0;
+-- find the first 0 entries of the table whose address is "Hyderabad".
+SELECT * FROM RGMLEARNERS WHERE STD_ADDRESS = "HYDERABAD" LIMIT 0;
+-- find the first two entries having College "KGIT" and the names are in ascending order.
 SELECT * FROM RGMLEARNERS WHERE STD_COLLEGE= "KGIT" ORDER BY STD_NAME LIMIT 2;
 
 CREATE TABLE DOB (
